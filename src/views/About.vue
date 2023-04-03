f1e299874c4e102e3bf58cf882961788673ae2e0
blacke4gle <ahmetdemirr0@gmail.com>
2023-04-03 11:20:29 +0300

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia officia
+      ea veritatis incidunt totam! Repellat libero molestias deleniti vel itaque
+      odio consequatur quod laboriosam, ad cumque laudantium cum eius
+      necessitatibus?
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia officia
+      ea veritatis incidunt totam! Repellat libero molestias deleniti vel itaque
+      odio consequatur quod laboriosam, ad cumque laudantium cum eius
+      necessitatibus?
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia officia
+      ea veritatis incidunt totam! Repellat libero molestias deleniti vel itaque
+      odio consequatur quod laboriosam, ad cumque laudantium cum eius
+      necessitatibus?
+    </p>
+  </div>
+</template>
